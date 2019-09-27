d044f91916a48ad2c8d2c3321848b27b02d201a1
Keegan D'Souza <keegan0415dsouza@gmail.com>
2019-09-27 15:22:38 -0500

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo hello thdere
+ln -s /node_modules /home/site/wwwroot/node_modules
+
+node --version
+npm --version
+ng serve
+ln -s /node_modules /home/site/wwwroot/node_modules
+echo linking 
